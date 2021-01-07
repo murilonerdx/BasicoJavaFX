25d891e06ff6965a0ad52cb3844a0d321959a748
n30config <mu-silva@outlook.com>
2021-01-07 18:01:23 -0300

    FXML

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML
+    private Button btTeste;
+
+    public void onBtTestAction(){
+        
+    }
+
 }
